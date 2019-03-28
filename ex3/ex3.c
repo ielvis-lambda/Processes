c24b64c5969b9cd5de85dc2a4386ee1521f6cb4c
Elvis Ibarra <ielvisd@gmail.com>
2019-03-28 14:03:02 -0700

    ex3 complete

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -9,7 +9,25 @@
 
 int main(void)
 {
-    // Your code here
+
+    // printf("hello world (pid: %d)\n", (int)getpid());
+
+    int rc = fork();
+    // ------------------------------------------------ child process starts executing here
+    if (rc < 0)
+    { // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0)
+    { // child process satisfies this branch
+        printf("hello, child here (pid: %d) \n", (int)getpid());
+    }
+    else
+    {
+        int wc = waitpid(rc, NULL, 0); // `waitpid` call added here
+        printf("goodbye, parent here (pid: %d) of child %d\n", (int)getpid(), rc);
+    }
 
     return 0;
 }
