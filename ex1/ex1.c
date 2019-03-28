15cff8d8a2d1d25ebc89a09581e3a5f2fba94fe1
Elvis Ibarra <ielvisd@gmail.com>
2019-03-28 11:41:25 -0700

    ex1.c is complete

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -5,10 +5,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main(void)
 {
-    // Your code here
+
+    // When the parent changes the value it changes in both the parent and the child. When the child changes the value it changes only in the child.
+    int x = 100;
+    //fork() returns twice
+    int pid = fork();
+
+    x = 5;
+
+    if (pid == 0)
+    {
+
+        x = 10;
+
+        printf("I'm x in child: %d\n", x);
+
+        exit(1);
+    }
+    else
+    {
+        wait(NULL);
+
+        printf("I'm x in parent: %d\n", x);
+    }
 
     return 0;
 }
