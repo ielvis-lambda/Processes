3d5e5dee753b01f4cf004a641d2a94fa2e7b2ecc
Elvis Ibarra <ielvisd@gmail.com>
2019-03-28 14:32:37 -0700

    ex4.c complete

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -3,6 +3,8 @@
 // `execl()`, `execle()`, `execv()`, and others. Why do you think there 
 // are so many variants of the same basic call?
 
+//Different calls for different types of arguments (what they are/point to)
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -10,7 +12,34 @@
 
 int main(void)
 {
-    // Your code here    
+
+    // printf("hello world (pid: %d)\n", (int)getpid());
+
+    int rc = fork();
+    // ------------------------------------------------ child process starts executing here
+    if (rc < 0)
+    { // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0)
+    { // child process satisfies this branch
+
+        //doesn't run it? 
+        // execl("ls", "ls", NULL);
+        
+        //doesn't run it? 
+        // execle("ls", "ls", NULL);
+        
+        execlp("ls", "ls", NULL);
+
+        printf("hello, child here (pid: %d) \n", (int)getpid());
+    }
+    else
+    {
+        int wc = waitpid(rc, NULL, 0); // `waitpid` call added here
+        printf("goodbye, parent here (pid: %d) of child %d\n", (int)getpid(), rc);
+    }
 
     return 0;
 }
